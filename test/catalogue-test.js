ec065b40a7eabfc8c9d02009c84e35d85b0fc850
Yiwei Liu <2814824250@qq.com>
2023-09-28 05:34:39 +0800

    Modify according to the solution.

--- test/catalogue-test.js
+++ test/catalogue-test.js
@@ -13,7 +13,7 @@ cat.addProduct(new Product("A124", "Product 2", 100, 10.0));
 cat.addProduct(new Product("A125", "Product 3", 100, 10, 10.0));
 
 describe("Catalogue", () => {
-    beforeEach( () => {
+    beforeEach(() => {
         cat = new Catalogue("Test Catalogue");
         cat.addProduct(new Product("A123", "Product 1", 100, 10, 10.0));
         cat.addProduct(new Product("A124", "Product 2", 100, 10.0));
@@ -45,26 +45,27 @@ describe("Catalogue", () => {
     });
     describe("checkReorder", () => {
         it("should return an empty array when no products need reordering", function () {
-          const result = cat.checkReorders();
-          expect(result.productIds).to.be.empty;
+            const result = cat.checkReorders();
+            expect(result.productIds).to.be.empty;
         });
         it("should report products that satisfy the reorder criteria", function () {
-          cat.addProduct(new Product("B123", "Product 4", 10, 20, 10.0));
-          cat.addProduct(new Product("B124", "Product 5", 10, 30, 10.0));
-          const result = cat.checkReorders();
-          expect(result.productIds).to.have.lengthOf(2);
-          expect(result.productIds).to.have.members(["B123", "B124"]);
+            cat.addProduct(new Product("B123", "Product 4", 10, 20, 10.0));
+            cat.addProduct(new Product("B124", "Product 5", 10, 30, 10.0));
+            const result = cat.checkReorders();
+            expect(result.productIds).to.have.lengthOf(2);
+            expect(result.productIds).to.have.members(["B123", "B124"]);
         });
         it("should include products just on their reorder level", function () {
-          cat.addProduct(new Product("B125", "Product 6", 10, 10, 10.0));
-          const result = cat.checkReorders();
-          // TO BE COMPLETED
-          expect(result.productIds).to.have.lengthOf(1);
-          expect(result.productIds).to.have.members(["B125"]);
+            cat.addProduct(new Product("B125", "Product 6", 10, 10, 10.0));
+            const result = cat.checkReorders();
+            // TO BE COMPLETED
+            expect(result.productIds).to.have.members(["B125"]);
         });
         it("should handle the empty catalogue case", function () {
-           // TO BE COMPLETED
-           expect(cat).to.be.empty;
-         });
-      });
+            // TO BE COMPLETED
+            cat = new Catalogue("Test catalogue");
+            const result = cat.checkReorders();
+            expect(result.productIds).to.be.empty;
+        });
+    });
 });
